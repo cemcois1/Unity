fbfbaf01722f333c1ebf71ea2ebe1e456958e24f
cemcois1 <cemalcan_yildirim10@hotmail.com>
2020-03-12 16:29:40 +0300

    Update attım

---
+++ Assets/asdsad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu (fileName ="deneme",menuName ="deneme2")]
+public class asdsad : ScriptableObject
+{
+    public int a;
+    public int ab;
+    public int ac;
+    public int ad;
+
+
+
+}
