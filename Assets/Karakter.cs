05257c8e800d1935418d37e6dd3e2507b6bcb2cb
cemal can yıldırım <56302638+cemcois1@users.noreply.github.com>
2020-03-17 17:38:31 +0300

    characterclass
    henüz bitmedi

---
+++ Assets/Karakter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Karakter : ScriptableObject
+{
+    private int health;
+    private string name;
+    private string tag;
+    private Dictionary<string, Animation> animationlist;
+    private Stats characterstats;
+    private Dictionary<string, AudioSource> soundList;
+    private GameObject karkater;
+    private Rigidbody rigidbody;
+    private Dictionary<string, Animation> effectoncharacter;
+}
---
+++ Assets/Stats.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : ScriptableObject
+{
+    private int maxHealth;
+    private int meleeDamage;
+    private int rangeDamage;
+    private int defance;
+    private int attackRange;
+    private int moveRange;
+    private int runRange;
+    private int completition = 100;
+    private int info;
+    private int meleeWeaponSkill;
+    private int rangeWeaponSkill;
+    private float regenerationSkill;
+
+    public void setmaxHealth(int Health) { maxHealth = Health; }
+    public int getMaxHealth() { return maxHealth;}
+
+    public void setMeleeDamage(int Damage) { meleeDamage = Damage; }
+    public int getMeleeDamage() { return meleeDamage; }
+
+    public void setRangeDamage(int Damage) { rangeDamage = Damage; }
+    public int getRangeDamage() { return rangeDamage; }
+
+
+    public void setDefance(int defance) { this.defance=defance ; }
+    public int getDefance() { return defance; }
+
+
+    public void setAttackRange(int range) { attackRange = range; }
+    public int getAttackrange() { return attackRange; }
+
+
+    public void setMoveRange(int range) { moveRange = range; }
+    public int getMoveRange() { return moveRange; }
+
+
+
+}
