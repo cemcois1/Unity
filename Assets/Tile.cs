49a0c7154e2c9bdb160e369131f2fd300c2b7492
IbrahimTahaSanli <59223514+IbrahimTahaSanli@users.noreply.github.com>
2020-03-19 17:16:17 +0300

    Update Tile.cs

--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -12,6 +12,7 @@ public struct inTile {
     public Cover cover;
     public Character character;
 }
+
 [CreateAssetMenu(fileName = "deneme", menuName = "deneme2")]
 public class Tile : ScriptableObject
 {
